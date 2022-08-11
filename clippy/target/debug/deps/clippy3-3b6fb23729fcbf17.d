62506b33f284b5676a87908a4785f6e6be0888a2
yo-kuma <yo-kuma@kddi.com>
2022-08-11 22:20:35 +0900

    first commit

---
+++ clippy/target/debug/deps/clippy3-3b6fb23729fcbf17.d
@@ -0,0 +1,7 @@
+/Users/yo-kuma/rustlings/exercises/clippy/target/debug/deps/clippy3-3b6fb23729fcbf17.rmeta: clippy3.rs
+
+/Users/yo-kuma/rustlings/exercises/clippy/target/debug/deps/clippy3-3b6fb23729fcbf17.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
